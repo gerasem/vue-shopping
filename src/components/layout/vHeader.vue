91912a289821a49c50da8086c165cee88430100e
Dmitri Laptev <dmitri.laptev@mtms.at>
2022-02-16 13:11:55 +0100

    header start html and css

--- src/components/layout/vHeader.vue
+++ src/components/layout/vHeader.vue
@@ -1,6 +1,45 @@
 <template>
-  <header>
-    Header
+  <header class="header__container">
+    <div class="container-xxl">
+      <div class="row">
+        <div class="col">
+          <router-link to="/" class="header__logo">Logo</router-link>
+          <span class="header__slogan">Lorem ipsum</span>
+        </div>
+
+        <div class="col-auto">
+          <div class="header__languages">
+            DE / EN
+          </div>
+        </div>
+
+        <div class="col-auto">
+          <router-link to="/" class="header__contact">
+            Contact
+          </router-link>
+        </div>
+
+        <div class="col-auto">
+          <div class="header__search">
+            <input type="text" class="form-input">
+          </div>
+        </div>
+
+        <div class="col-auto">
+          <div class="header__icons">
+            <router-link to="/">
+              <i class="icon bi bi-person" style="font-size: 28px"></i>
+            </router-link>
+            <router-link to="/">
+              <i class="icon bi bi-heart"></i>
+            </router-link>
+            <router-link to="/">
+              <i class="icon bi bi-bag"></i>
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
   </header>
 </template>
 
@@ -10,9 +49,7 @@ import {useI18n} from 'vue-i18n'
 export default {
   props: {},
 
-  components: {
-
-  },
+  components: {},
 
 
   setup() {
@@ -27,9 +64,15 @@ export default {
 </script>
 
 <style scoped lang="scss">
-header {
-  height: 100px;
-  width: 100%;
-  padding: 15px;
+
+.header{
+  &__container{
+    height: 100px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    padding: 0 15px;
+  }
 }
+
 </style>
