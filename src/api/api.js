0b873e24b3f444b23afe3d4c849d504d6ef2b152
Dmitri Laptev <dmitri.laptev@sidecaps.com>
2022-10-03 23:13:57 +0200

    scss new path

--- src/api/api.js
+++ src/api/api.js
@@ -1,31 +1,31 @@
 import axios from "axios";
 
 const instance = axios.create({
-    //baseURL: 'http://0.0.0.0:3001',
-    headers: {
-        'Content-Type': 'application/json',
-    },
+  //baseURL: 'http://0.0.0.0:3001',
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-import categories from "./mock/categories.json"
-import items from "./mock/items.json"
-import coupons from "./mock/coupons.json"
+import categories from "./mock/categories.json";
+import items from "./mock/items.json";
+import coupons from "./mock/coupons.json";
 
 export const dataAPI = {
-    getCategories() {
-        return categories;
-    },
+  getCategories() {
+    return categories;
+  },
 
-    getAllItems(id) {
-        return items;
-    },
+  getAllItems() {
+    return items;
+  },
 
-    checkCoupon(couponCode) {
-        if (coupons[couponCode]) {
-            console.log('coupon is in list');
-            return coupons[couponCode];
-        } else {
-            return false;
-        }
+  checkCoupon(couponCode) {
+    if (coupons[couponCode]) {
+      console.log("coupon is in list");
+      return coupons[couponCode];
+    } else {
+      return false;
     }
-}
+  },
+};
